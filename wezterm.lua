e468d6579f7770fad1c98627514e46438b618159
Lesords <lesords168@gmail.com>
2026-06-28 00:29:59 +0800

    refactor(wezterm): remove backdrop and right-status module initialization

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,6 @@
 local Config = require('config')
 
-require('utils.backdrops')
-   -- :set_images_dir(require('wezterm').home_dir .. '/Pictures/Wallpapers/')
-   :scan_images_dir()
-   :random()
-
 require('events.left-status').setup()
-require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
 require('events.tab-title').setup({
    hide_active_tab_unseen = true,
    unseen_icon = 'numbered_box',
